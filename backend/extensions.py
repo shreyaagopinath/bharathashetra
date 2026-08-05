f40aafb507cc68fadd33c9adc4890f920c03b548
shreyaagopinath <shreyaa.gopinath@gmail.com>
2026-08-05 14:50:18 -0500

    Set JWT secrets at module level before JWT initialization

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,5 +1,12 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+
+# Ensure JWT secrets are set
+if 'JWT_SECRET_KEY' not in os.environ:
+    os.environ['JWT_SECRET_KEY'] = 'bharathashetra-jwt-secret-key-dev-2024'
+if 'SECRET_KEY' not in os.environ:
+    os.environ['SECRET_KEY'] = 'bharathashetra-secret-key-dev-2024'
 
 db = SQLAlchemy()
 jwt = JWTManager()
